cdee172aa6e4ec34ac868ca748879e04bad2df6c
Tom Schindl <tom.schindl@bestsolution.at>
2014-09-28 06:03:37 -0700

    setting google key

--- at.bestsolution.di.services.google/src/at/bestsolution/di/services/google/GoogleTranslate.java
+++ at.bestsolution.di.services.google/src/at/bestsolution/di/services/google/GoogleTranslate.java
@@ -24,7 +24,7 @@ import org.json.JSONObject;
 import at.bestsolution.di.services.TranslationService;
 
 public class GoogleTranslate implements TranslationService {
-	private String API_KEY;
+	private String API_KEY = "AIzaSyCYJ8Aqt0oEy0xbkyaJibKSk0akAR46x5I";
 	
 	// https://developers.google.com/translate/v2/using_rest#language-params
 	
